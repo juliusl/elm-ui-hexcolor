703bdc9d61d56e49d2bb76b602f5d1558d36751b
Julius Liu <juliusl@microsoft.com>
2022-04-01 18:42:07 -0700

    Doc. Adding doc comments

--- src/Element/HexColor.elm
+++ src/Element/HexColor.elm
@@ -1,12 +1,19 @@
-module Element.HexColor exposing (rgbaHex, rgbHex)
+module Element.HexColor exposing (rgbaCSSHex, rgbCSSHex)
 
 import Element exposing (Color, rgba255, rgb255)
 import ColorHelper
 
-rgbaHex: String -> Color
-rgbaHex hexcode = 
+{-| Convert a CSS Hex color into an elm-ui.Element.Color with rgba255 
+    rgbaCSSHex "#7f11e088"
+-}
+rgbaCSSHex: String -> Color
+rgbaCSSHex hexcode = 
     ColorHelper.rgbaFromString hexcode rgba255
 
-rgbHex: String -> Color
-rgbHex hexcode = 
+
+{-| Convert a CSS Hex color into an elm-ui.Element.Color with rgb255 
+    rgbCSSHex "#7f11E0"
+-}
+rgbCSSHex: String -> Color
+rgbCSSHex hexcode = 
     ColorHelper.rgbFromString hexcode rgb255
